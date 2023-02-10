40351a21c8f1be2a49488e3e668b61d9067df061
MandeepMann <mandeep_mann30@hotmail.com>
2023-02-10 18:04:00 -0500

    log in/out works

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import './App.css';
 import { Routes, Route, Link } from 'react-router-dom';
+import { useState } from 'react';
+import { auth } from './firebase.js'
+import { signOut } from 'firebase/auth'
 import Home from './pages/Home.js';
 import Login from './pages/Login.js';
 import CreatePost from './pages/CreatePost.js';
@@ -7,6 +10,18 @@ import CreatePost from './pages/CreatePost.js';
 import dandelion from './assets/dandelion.png';
 
 function App() {
+
+    const [isAuth, setIsAuth] = useState(false);
+
+    const signUserOut = () => {
+        signOut(auth)
+        .then(() => {
+            localStorage.clear();
+            setIsAuth(false);
+            window.location.pathname = '/login'
+        })
+    }
+
     return (
         <div className="App">
             <header>
@@ -16,14 +31,15 @@ function App() {
                     </Link>
                     <nav>
                         <Link to='/'>Home</Link>
-                        <Link to='/login'>Log In</Link>
+                        {/* Removes login button if we are logged in */}
+                        {!isAuth ? <Link to='/login'>Log In</Link> : <button onClick={signUserOut}>Log Out</button>}
                         <Link to='/createpost'>New Post</Link>
                     </nav>
                 </div>
             </header>
             <Routes>
                 <Route path='/' element={<Home />} />
-                <Route path='/login' element={<Login />} />
+                <Route path='/login' element={<Login setIsAuth={setIsAuth}/>} />
                 <Route path='/createpost' element={<CreatePost />} />
             </Routes>
         </div>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,25 @@
-const Login = () => {
+import { auth, provider } from '../firebase.js';
+import { signInWithPopup } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
+
+const Login = ({ setIsAuth }) => {
+
+    let navigate = useNavigate();
+
+    const signInWithGoogle = () => {
+        signInWithPopup(auth, provider)
+        .then((result) => {
+            // allows user to stay logged in even when they've closed the tab and then come back
+            localStorage.setItem("isAuth", true);
+            setIsAuth(true);
+            navigate('/')
+        });
+    }
+
     return (
-        <div>
-            <h1>Login</h1>
+        <div className="loginPage">
+            <h1>Sign In With Google to Continue</h1>
+            <button className="loginButton" onClick={signInWithGoogle}>Sign in with Google</button>
         </div>
     )
 }
