eeea92e79abaa43435f36044fed7444f45fd4e30
MandeepMann <mandeep_mann30@hotmail.com>
2023-02-10 17:38:13 -0500

    routing done and a little styling

--- src/App.js
+++ src/App.js
@@ -4,9 +4,23 @@ import Home from './pages/Home.js';
 import Login from './pages/Login.js';
 import CreatePost from './pages/CreatePost.js';
 
+import dandelion from './assets/dandelion.png';
+
 function App() {
     return (
         <div className="App">
+            <header>
+                <div className="wrapper headerFlex">
+                    <Link to='/'>
+                        <img src={dandelion} alt="a dandelion" />
+                    </Link>
+                    <nav>
+                        <Link to='/'>Home</Link>
+                        <Link to='/login'>Log In</Link>
+                        <Link to='/createpost'>New Post</Link>
+                    </nav>
+                </div>
+            </header>
             <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='/login' element={<Login />} />
